115653a1c621571d154dbca35ca2a0f4164b5313
ADPsmackskeys <130333388+ADPsmackskeys@users.noreply.github.com>
2025-05-30 21:25:44 +0530

    Create roots.c

---
+++ shell-injection/roots.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	char buffer[256];
+	printf("%p\n", buffer);  
+	gets(buffer);        	
+	printf("%s\n", buffer);
+	return 0;
+}
